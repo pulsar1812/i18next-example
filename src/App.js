fabeee4573c3ddf9325939e796a68606e242daae
pulsar1812 <cluster000@gmail.com>
2019-09-26 17:00:12 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useTranslation } from 'react-i18next';
+
 import './App.css';
 
 function App() {
+  const [t, i18n] = useTranslation();
+
+  const handleClick = language => {
+    i18n.changeLanguage(language);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <nav
+        style={{ width: '100%', padding: '2rem, 0', backgroundColor: 'gray' }}
+      >
+        <button onClick={() => handleClick('en-US')}>English</button>
+        <button onClick={() => handleClick('zh-HK')}>Chinese</button>
+      </nav>
+      <p>{t('movie.quote')}</p>
     </div>
   );
 }
